e8e08c7cd5816bdb30268eb80475cf8490a74374
maxkizi <maxkizi@yandex.ru>
2025-03-29 13:46:56 +0300

    remove RegionService interface

--- src/main/kotlin/com/example/plugins/DependencyInjection.kt
+++ src/main/kotlin/com/example/plugins/DependencyInjection.kt
@@ -3,7 +3,6 @@ package com.example.plugins
 import com.example.repository.RegionInMemoryRepository
 import com.example.repository.RegionRepository
 import com.example.service.RegionService
-import com.example.service.RegionServiceImpl
 import io.ktor.server.application.*
 import org.koin.core.module.dsl.bind
 import org.koin.core.module.dsl.singleOf
@@ -13,7 +12,7 @@ import org.koin.ktor.plugin.Koin
 fun Application.configureDependencyInjection() {
     install(Koin) {
         val module = module {
-            singleOf(::RegionServiceImpl){bind<RegionService>()}
+            singleOf(::RegionService)
             singleOf(::RegionInMemoryRepository){bind<RegionRepository>()}
         }
         modules(module)
